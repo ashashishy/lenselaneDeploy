8cce02da39fb82a52148da9d40973e60498de87e
ashashishy <ash.ashish.y@gmail.com>
2020-08-13 18:03:02 +0530

    deploy 0.1

---
+++ precache-manifest.9d6fd25ffe2b9ede00e135e1b9d834c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec0e4a4f1ea13ee1f30c888c3d78d1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de4edc833e414ba7aaf2",
+    "url": "/static/css/main.a32b3c54.chunk.css"
+  },
+  {
+    "revision": "08b8013caf0113c09d90",
+    "url": "/static/js/2.4a39f752.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.4a39f752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4edc833e414ba7aaf2",
+    "url": "/static/js/main.b2ab53e3.chunk.js"
+  },
+  {
+    "revision": "6bedc7d4e72b37a14961",
+    "url": "/static/js/runtime-main.059467c3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
